85962f42ac4b4d2888f591a447ff4a85c26c223e
WiiDevYWiiDevYouSef <Youssef.Maslohi@outlook.com>
2025-07-02 15:33:25 +0200

    creating the blog content

--- src/components/pages/blog/blog.jsx
+++ src/components/pages/blog/blog.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import '../../../components/pages/blog/blog.css'
+import blogFirstImg from '../../../assets/images/blogImages/third-collection.jpg'
 
 const Blog = () => {
     return (
-        <div>
-            
+        <>
+        <div className="blog-container">
+            <div className="">
+                <img src={blogFirstImg} alt="" />
+                <h1>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit, facere.</h1>
+                <p>by <b>fash-them Admin on Dec 29,2025</b></p>
+            </div>
         </div>
+            
+        </>
     );
 };
 
