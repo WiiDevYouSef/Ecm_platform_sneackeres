4e2bdf316650fea91e227200bdd9842f93c4661c
WiiDevYWiiDevYouSef <Youssef.Maslohi@outlook.com>
2025-07-02 15:44:31 +0200

    blog card

--- src/components/pages/blog/blog.jsx
+++ src/components/pages/blog/blog.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import '../../../components/pages/blog/blog.css'
 import blogFirstImg from '../../../assets/images/blogImages/third-collection.jpg'
+import blogSecondImg from '../../../assets/images/blogImages/kobraCollectionImg.jpg'
 
 const Blog = () => {
     return (
         <>
         <div className="blog-container">
-            <div className="">
+            <div className="blog-card">
+                <img src={blogFirstImg} alt="" />
+                <h1>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit, facere.</h1>
+                <p>by <b>fash-them Admin on Dec 29,2025</b></p>
+            </div>
+            <div className="blog-card">
+                <img src={blogSecondImg} alt="" />
+                <h1>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit, facere.</h1>
+                <p>by <b>fash-them Admin on Dec 29,2025</b></p>
+            </div>
+            <div className="blog-card">
                 <img src={blogFirstImg} alt="" />
                 <h1>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit, facere.</h1>
                 <p>by <b>fash-them Admin on Dec 29,2025</b></p>
